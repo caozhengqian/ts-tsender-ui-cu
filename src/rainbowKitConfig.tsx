2837152c387525ecf61d23760432b8a3fa57aca8
caozhengqian <1264419544@qq.com>
2026-09-11 01:12:16 +0800

    add heard和彩虹按钮

---
+++ src/rainbowKitConfig.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import { getDefaultConfig } from "@rainbow-me/rainbowkit"
+import { arbitrum, base, mainnet, optimism, anvil, zksync, sepolia } from "wagmi/chains"
+export default getDefaultConfig({
+    appName: "TSender",
+    projectId:process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+    chains:[anvil,zksync,mainnet],
+    ssr:false
+})
